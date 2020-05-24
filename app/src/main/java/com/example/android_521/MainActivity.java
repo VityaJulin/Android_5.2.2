135f016a3cadb25c10a502ecb634d48188a5f209
vityajulin <vkdvornokov@gmail.com>
2020-05-24 18:35:06 +0300

    add external file

--- app/src/main/java/com/example/android_521/MainActivity.java
+++ app/src/main/java/com/example/android_521/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android_521;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -10,9 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -22,99 +29,178 @@ public class MainActivity extends AppCompatActivity {
     private EditText passwordEdTxt;
     private Button loginBtn;
     private Button registrationBtn;
+    private CheckBox chBoxExternalStorage;
+    private SharedPreferences chBoxChoice;
+    private final String PREF_STORAGE_MODE = "PREF_STORAGE_MODE";
+    private final String PREF_EXTERNAL_STORAGE = "PREF_EXTERNAL_STORAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialView();
+        chBoxChoice = getSharedPreferences(PREF_STORAGE_MODE, MODE_PRIVATE);
 
+
+        chBoxExternalStorage.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = chBoxChoice.edit();
+                editor.putBoolean(PREF_EXTERNAL_STORAGE, chBoxExternalStorage.isChecked());
+                editor.apply();
+            }
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FileInputStream loginInputStream = null;
-                FileInputStream passwordInputStream = null;
-
-                try {
-                    loginInputStream = openFileInput("login.txt");
-                    passwordInputStream = openFileInput("password.txt");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                if (chBoxChoice.getBoolean(PREF_EXTERNAL_STORAGE, false)) {
                 }
-
-                InputStreamReader loginStreamReader = new InputStreamReader(loginInputStream);
-                InputStreamReader passwordStreamReader = new InputStreamReader(passwordInputStream);
-
-                BufferedReader loginReader = new BufferedReader(loginStreamReader);
-                BufferedReader passwordReader = new BufferedReader(passwordStreamReader);
-
-                if (isInputFieldEmpty()) {
-                    Toast.makeText(MainActivity.this, R.string.error_input_empty, Toast.LENGTH_SHORT).show();
-                } else {
-                    String enterLogin = loginEdTxt.getText().toString();
-                    String enterPassword = passwordEdTxt.getText().toString();
-                    try {
-                        String userLogin = loginReader.readLine();
-                        String userPassword = passwordReader.readLine();
-                        if (userLogin == null || userPassword == null) {
-                            Toast.makeText(MainActivity.this, R.string.toast_not_registered, Toast.LENGTH_SHORT).show();
-                        } else {
-                            if (userLogin.equals(enterLogin) && userPassword.equals(enterPassword)) {
-                                Toast.makeText(MainActivity.this, R.string.toast_enter_ok, Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(MainActivity.this, R.string.toast_enter_failed, Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                readInnerStorage();
             }
+
+
         });
 
         registrationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FileOutputStream loginOutputStream = null;
-                FileOutputStream passwordOutputStream = null;
-
-                try {
-                    loginOutputStream = openFileOutput("login.txt", MODE_PRIVATE);
-                    passwordOutputStream = openFileOutput("password.txt", MODE_PRIVATE);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                OutputStreamWriter loginOutputStreamWriter = new OutputStreamWriter(loginOutputStream);
-                OutputStreamWriter passwordOutputStreamWriter = new OutputStreamWriter(passwordOutputStream);
-                BufferedWriter loginBw = new BufferedWriter(loginOutputStreamWriter);
-                BufferedWriter passwordBw = new BufferedWriter(passwordOutputStreamWriter);
-
-                if (isInputFieldEmpty()) {
-                    Toast.makeText(MainActivity.this, R.string.error_input_empty, Toast.LENGTH_SHORT).show();
+                if (chBoxChoice.getBoolean(PREF_EXTERNAL_STORAGE, false)) {
+                    writeExternalStorage();
                 } else {
-                    String login = loginEdTxt.getText().toString();
-                    String password = passwordEdTxt.getText().toString();
-                    try {
-                        loginBw.write(login);
-                        passwordBw.write(password);
-                        Toast.makeText(MainActivity.this, R.string.toast_registration_ok, Toast.LENGTH_SHORT).show();
-                        loginEdTxt.setText("");
-                        passwordEdTxt.setText("");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    try {
-                        loginBw.close();
-                        passwordBw.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    writeInnerStorage();
                 }
             }
+
+
         });
+    }
+
+    public void writeExternalStorage() {
+        if (isExternalStorageWritable() == true) {
+            File saveData = new File(getApplicationContext()
+                    .getExternalFilesDir(null), "save.txt");
+
+            String enterLogin = loginEdTxt.getText().toString();
+            String enterPassword = passwordEdTxt.getText().toString();
+
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(saveData, true);
+                writer.append(enterLogin + "\n" + enterPassword);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            Toast.makeText(this, "No access", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void readExternalStorage() {
+        if (isExternalStorageWritable() == true) {
+            File saveData = new File(getApplicationContext()
+                    .getExternalFilesDir(null), "save.txt");
+
+
+            FileReader reader = null;
+
+            try {
+                reader = new FileReader(saveData);
+                String userLogin = reader.readline;
+                String userPassword = reader.readLine();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "No access", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void readInnerStorage() {
+        FileInputStream loginInputStream = null;
+        FileInputStream passwordInputStream = null;
+
+        try {
+            loginInputStream = openFileInput("login.txt");
+            passwordInputStream = openFileInput("password.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        InputStreamReader loginStreamReader = new InputStreamReader(loginInputStream);
+        InputStreamReader passwordStreamReader = new InputStreamReader(passwordInputStream);
+
+        BufferedReader loginReader = new BufferedReader(loginStreamReader);
+        BufferedReader passwordReader = new BufferedReader(passwordStreamReader);
+
+        if (isInputFieldEmpty()) {
+            Toast.makeText(MainActivity.this, R.string.error_input_empty, Toast.LENGTH_SHORT).show();
+        } else {
+            String enterLogin = loginEdTxt.getText().toString();
+            String enterPassword = passwordEdTxt.getText().toString();
+            try {
+                String userLogin = loginReader.readLine();
+                String userPassword = passwordReader.readLine();
+                if (userLogin == null || userPassword == null) {
+                    Toast.makeText(MainActivity.this, R.string.toast_not_registered, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (userLogin.equals(enterLogin) && userPassword.equals(enterPassword)) {
+                        Toast.makeText(MainActivity.this, R.string.toast_enter_ok, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, R.string.toast_enter_failed, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void writeInnerStorage() {
+        FileOutputStream loginOutputStream = null;
+        FileOutputStream passwordOutputStream = null;
+
+        try {
+            loginOutputStream = openFileOutput("login.txt", MODE_PRIVATE);
+            passwordOutputStream = openFileOutput("password.txt", MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        OutputStreamWriter loginOutputStreamWriter = new OutputStreamWriter(loginOutputStream);
+        OutputStreamWriter passwordOutputStreamWriter = new OutputStreamWriter(passwordOutputStream);
+        BufferedWriter loginBw = new BufferedWriter(loginOutputStreamWriter);
+        BufferedWriter passwordBw = new BufferedWriter(passwordOutputStreamWriter);
+
+        if (isInputFieldEmpty()) {
+            Toast.makeText(MainActivity.this, R.string.error_input_empty, Toast.LENGTH_SHORT).show();
+        } else {
+            String login = loginEdTxt.getText().toString();
+            String password = passwordEdTxt.getText().toString();
+            try {
+                loginBw.write(login);
+                passwordBw.write(password);
+                Toast.makeText(MainActivity.this, R.string.toast_registration_ok, Toast.LENGTH_SHORT).show();
+                loginEdTxt.setText("");
+                passwordEdTxt.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                loginBw.close();
+                passwordBw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public boolean isInputFieldEmpty() {
@@ -124,7 +210,11 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    public boolean isNoSaveData() {
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
         return false;
     }
 
@@ -133,5 +223,6 @@ public class MainActivity extends AppCompatActivity {
         passwordEdTxt = findViewById(R.id.edtxt_password);
         loginBtn = findViewById(R.id.btn_login);
         registrationBtn = findViewById(R.id.btn_registration);
+        chBoxExternalStorage = findViewById(R.id.chbox_external_storage);
     }
 }
